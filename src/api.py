2ae8b241a2a2ed85e4a827407593c3520b237a2f
yurtema <yurtema@gmail.com>
2023-07-12 12:05:14 +0300

    Небольшие изменения, добавил комментарии

--- src/api.py
+++ src/api.py
@@ -7,24 +7,31 @@ async def parse_modrinth(id_, loader_):
     versions = []
     link = f'https://api.modrinth.com/v2/project/{id_}/version?loaders=["{loader_}"]'
 
+    # Сделать асинхронный запрос на апи модринха
     loop = asyncio.get_event_loop()
     r = (await loop.run_in_executor(None, requests.get, link)).json()
 
+    # Апи вернет структуру [{.... game_versions:[] .....},
+    #                       {.... game_versions:[] .....}]
+    # Из этого надо вытащить список всех доступных версий
     for version in r:
         versions += version['game_versions']
 
-    return {id_: list(set(versions))}
+    # И вернуть словарь нужного формата с удалением дубликатов
+    return {id_: set(versions)}
 
 
 async def create_processes(data: dict):
+    """Создать асинхронные процессы для каждого элемента из полученного словаря типа {id мода: требуемый загрузчик}"""
     tasks = []
-    result_dict = {}
 
+    # Получить result_list, список словарей с результатами запросов типа [{id мода: [версии]}, {id мода: [версии]}]
     for mod_id in data:
         tasks.append(asyncio.create_task(parse_modrinth(mod_id, data[mod_id])))
-
     result_list = await asyncio.gather(*tasks)
+
+    # Вот тут список словарей превращается в один большой словарь
+    result_dict = {}
     [result_dict.update(i) for i in result_list]
 
     return result_dict
-
--- src/tg_main.py
+++ src/tg_main.py
@@ -12,30 +12,46 @@ with open("../data/data.json") as file:
 
 while True:
 
+    # Если последняя проверка была меньше часа назад, подождать еще минуту
     if last_check_hour == time.localtime().tm_hour:
         time.sleep(60)
         continue
     last_check_hour = time.localtime().tm_hour
 
     mods = {}
-    mods_to_remove = {}
 
+    # Создать словарь типа {id мода: требуемый загрузчик}
     for user in data:
         for mod in data[user]:
             mods.update({data[user][mod]['id']: data[user][mod]['loader']})
 
+    # Передать этот словарь в api.py, получить словарь типа {id мода: суещствующие версии}
     versions = asyncio.run(api.create_processes(mods))
 
+    mods_to_remove = {}
+    mods_to_remove_for_user = []
+
     for user in data:
+
         for mod in data[user]:
+            # Для каждого мода каждого пользователя проверить, есть ли требуемая версия в списке доступных
             if data[user][mod]["version"] in versions[data[user][mod]["id"]]:
+                # Если есть, кинуть сообщение пользователю и добавить мод в список на удаление из бд
                 requests.post(
                     f'https://api.telegram.org/bot{private.tg_token}/sendMessage?chat_id={private.tg_chat_id}&text='
                     f'Мод {mod} из Вашего списка ожидания обновили на {data[user][mod]["version"]}')
-                mods_to_remove.update({mod: user})
+                mods_to_remove_for_user.append(mod)
 
-    for i in mods_to_remove:
-        del data[mods_to_remove[i]][i]
+        # Создать словарь тиа {пользователь: [список модов, которые надо удалить из бд]}
+        mods_to_remove[user] = mods_to_remove_for_user
+        mods_to_remove_for_user = []
+
+    # Удалить все моды из списка
+    for user in mods_to_remove:
+        for mod in mods_to_remove[user]:
+            del data[user][mod]
 
     with open("../data/data.json", 'w') as file:
         json.dump(data, file)
+
+

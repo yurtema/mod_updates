6c513091d549a4a140086d08c1b90a926c72822a
yurtema <yurtema@gmail.com>
2023-08-01 12:00:17 +0300

    небольшие финальные изменения

--- src/tg_main.py
+++ src/tg_main.py
@@ -3,9 +3,8 @@ import time
 import asyncio
 import requests
 import urllib3.exceptions
-
 from updates_manager import responde
-import api
+from api import create_processes
 
 last_check_hour = None
 
@@ -29,6 +28,8 @@ def handle_updates():
 
     global offset
 
+    # Попробовать тыкнуть апи телеги, чтобы получить новые сообщения.
+    # Если чето пойдет не так, подождать минуту и тыкнуть снова.
     try:
         update = requests.get(f'https://api.telegram.org/bot{private["tg_token"]}/'
                               f'getUpdates?offset={offset}').json()['result']
@@ -36,8 +37,9 @@ def handle_updates():
         time.sleep(60)
         return
 
+    # Если никаких новых сообщений нет, ничего не делать (нет, что вы, какая излишняя документация?)
     if not update:
-        return None
+        return
 
     update = update[0]
     offset = update['update_id'] + 1
@@ -46,12 +48,10 @@ def handle_updates():
 
     print(f'[{time.asctime()}] {author_id}: {text}')
 
+    # Прокатить команду по responde для получения строчки с ответом и отправить запрос на апи для публикации сообщения.
     requests.post(f'https://api.telegram.org/bot{private["tg_token"]}/sendMessage?chat_id={author_id}&'
                   f'text={responde(text, author_id)}')
 
-
-with open("../data/data.json") as file:
-    data = json.load(file)
 
 while True:
 
@@ -59,19 +59,22 @@ while True:
     if last_check_hour == time.localtime().tm_hour:
         handle_updates()
         continue
-    last_check_hour = time.localtime().tm_hour
 
-    mods = {}
+    # Если больше, запустить проверку обновлений для модов
+
+    # Загруить список запрашиваемых модов
+    with open("../data/data.json") as file:
+        data = json.load(file)
 
     # Создать словарь типа {id мода: требуемый загрузчик}
-    for user in data:
-        for mod in data[user]:
-            mods.update({data[user][mod]['id']: data[user][mod]['loader']})
+    mods = {data[user][mod]['id']: data[user][mod]['loader'] for user in data for mod in data[user]}
 
-    # Передать этот словарь в api.py, получить словарь типа {id мода: суещствующие версии}
-    versions = asyncio.run(api.create_processes(mods))
+    # Передать этот словарь в api.py, получить словарь типа {id мода: [существующие версии]}
+    versions = asyncio.run(create_processes(mods))
 
+    # {пользователь: [список модов, которые надо удалить из бд]}
     mods_to_remove = {}
+    # временынй список, создаваемый для каждого пользователя
     mods_to_remove_for_user = []
 
     for user in data:
@@ -89,11 +92,12 @@ while True:
         mods_to_remove[user] = mods_to_remove_for_user
         mods_to_remove_for_user = []
 
-    # Удалить все моды из списка
+    # Удалить все нужные моды из списка
     for user in mods_to_remove:
         for mod in mods_to_remove[user]:
             del data[user][mod]
 
+    # Если чето удалили, обновить файл
     if mods_to_remove != {}:
         with open("../data/data.json", 'w') as file:
             json.dump(data, file)
